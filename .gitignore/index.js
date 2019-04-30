39d9383ae4f77e3ac993d8003596780e398bf2b6
Mangouste57 <jojo.gimbel@gmail.com>
2019-04-30 17:49:17 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,9 @@ const bot = new Discord.Client()
 bot.on('message',function(message){
 console.log(message.member.user.tag);
     if(message.member.user.tag === "Nico ksx98#0787"){
-        message.channel.send(":eyes",{files:["test.jpg"]})
-
+        message.channel.send(":eyes:",{files:["test.jpg"]})
+    if(message.member.user.tag === "MangousteJR#6098"){
+        message.channel.send(":eyes:")
         
     }
 })
